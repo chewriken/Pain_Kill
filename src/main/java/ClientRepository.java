ad9de131f17f8686f2b5a07a130210a943b3f57e
chewriken <46991620+chewriken@users.noreply.github.com>
2020-11-06 10:12:20 +0100

    Création du porjet
    ajout des différents fichier pour charger correctement le projet

---
+++ src/main/java/ClientRepository.java
@@ -0,0 +1,9 @@
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Long> {
+
+    List<Client> findByLastName(String lastName);
+    Client findById(long id);
+}
